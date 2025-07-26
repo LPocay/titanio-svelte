2f7b37f6c493cec30c28e76150571725301894e8
Luigi Pocay <luigiapocay92@gmail.com>
2025-07-26 17:39:54 -0300

    Adds product section for desktop

--- src/lib/LinkButton.svelte
+++ src/lib/LinkButton.svelte
@@ -5,7 +5,7 @@
 <div>
 	<a
 		href=""
-		class="flex w-fit gap-1 rounded-3xl border border-solid border-white p-3 font-inter text-xs whitespace-nowrap text-white"
+		class="flex w-fit gap-1 md:gap-2 rounded-3xl border border-solid border-white p-3 font-inter text-xs whitespace-nowrap text-white md:text-[16px]"
 	>
 		{@render children()}
 		<img src="/img/button-arrow.svg" alt="" />
--- src/lib/Product.svelte
+++ src/lib/Product.svelte
@@ -4,14 +4,19 @@
 </script>
 
 <section
-	class="relative h-screen w-screen bg-[url(/img/product-bg.png)] px-8 pt-28 pb-8 font-inter text-white flex flex-col"
+	class="relative flex h-screen w-screen flex-col bg-[url(/img/product-bg.png)] px-8 pt-28 pb-8 md:p-0 font-inter text-white md:flex-row md:items-center md:justify-between md:bg-[url(/img/product-big-bg.png)] md:bg-cover"
 	id="producto"
 >
 	<ScrollArrow position="left" />
-	<div class="mb-4">
-		<h1 class="text-xs">El producto</h1>
-		<p class="text-2xl">Titanio es un barral para proteger tu hogar, a un precio accesible.</p>
+	<div class="md:flex md:items-start md:justify-center md:basis-[61%] md:flex-col md:w-full md:px-[15vw]">
+		<div class="mb-4">
+			<h1 class="text-xs md:text-[16px]">El producto</h1>
+			<p class="text-2xl md:text-3xl">Titanio es un barral para proteger tu hogar, a un precio accesible.</p>
+		</div>
+		<LinkButton>Ver especificaciones</LinkButton>
 	</div>
-	<LinkButton>Ver especificaciones</LinkButton>
-  <div class="bg-[url(https://placehold.co/600x400)] h-full w-full max-w-[230px] mx-auto bg-center mt-16 rounded-[120px]"></div>
+	<div
+		class="mx-auto mt-16 h-full w-full max-w-[230px] rounded-[120px] bg-[url(https://placehold.co/600x400)] bg-center md:hidden"
+	></div>
+	<div class="bg-[url(https://placehold.co/600x400)] h-full basis-[40%] max-sm:hidden"></div>
 </section>

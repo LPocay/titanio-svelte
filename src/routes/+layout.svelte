d2f7c7c2f4c454a08b692157e1096b9a348433aa
Luigi Pocay <luigiapocay92@gmail.com>
2025-07-26 16:59:44 -0300

    Adds Lenis lib for smooth scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import '../app.css';
+	import Lenis from 'lenis';
 
 	let { children } = $props();
+
+	if (browser) {
+		new Lenis({
+			autoRaf: true
+		});
+	}
 </script>
 
 {@render children()}

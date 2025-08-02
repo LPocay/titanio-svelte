7edd967f5e9ba53e6e4d306d6ed07fdb554a01c0
Luigi Pocay <luigiapocay92@gmail.com>
2025-08-02 17:33:51 -0300

    Adds intro to product page

---
+++ src/lib/product/ProductIntro.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Nav from '$lib/Nav.svelte';
+	import ScrollArrow from '../ScrollArrow.svelte';
+</script>
+
+<section
+	class="relative h-screen bg-[url(/img/intro-bg.png)] bg-cover md:overflow-hidden md:bg-[url(/img/intro-big-bg.png)]"
+>
+	<Nav />
+	<ScrollArrow position="right" />
+	<div
+		class="flex h-full flex-col items-center justify-center pt-1 text-white md:items-start md:justify-end md:pl-14 md:absolute md:bottom-[-30px]"
+	>
+		<h1 class="font-inter md:text-[44px]">Como funciona</h1>
+		<h1 class="font-gabarito text-6xl font-extrabold uppercase md:text-[180px]">Titanio</h1>
+	</div>
+</section>
--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import ProductIntro from '$lib/product/ProductIntro.svelte';
 </script>
 
 <main>
+  <ProductIntro />
 </main>

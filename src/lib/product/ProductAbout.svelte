135a8bb6775fd6fa135deabdee0397b265bd96b7
Luigi Pocay <luigiapocay92@gmail.com>
2025-08-02 17:44:21 -0300

    Adds initial product about section

---
+++ src/lib/product/ProductAbout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import ScrollArrow from '../ScrollArrow.svelte';
+</script>
+
+<section
+	class="relative flex h-screen w-full flex-col bg-[url(/img/product-bg.png)] px-8 pt-28 pb-8 font-inter text-white md:flex-row md:items-center md:justify-between md:bg-[url(/img/product-big-bg.png)] bg-cover md:p-0"
+	id="producto"
+>
+	<ScrollArrow position="left" />
+</section>
--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import ProductAbout from '$lib/product/ProductAbout.svelte';
 	import ProductIntro from '$lib/product/ProductIntro.svelte';
 </script>
 
 <main>
   <ProductIntro />
+  <ProductAbout />
 </main>

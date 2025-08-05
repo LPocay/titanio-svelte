ffa1665de11d12441c3d4a80b28b6dac45c6fad8
Luigi Pocay <luigiapocay92@gmail.com>
2025-08-04 23:26:00 -0300

    Adds QUality section

---
+++ src/lib/product/Quality.svelte
@@ -0,0 +1,21 @@
+<script>
+	import ScrollArrow from '../ScrollArrow.svelte';
+</script>
+
+<section class="relative grid h-screen w-full grid-rows-2 text-titanio-500" id="quality">
+	<div
+		class="relative flex w-full flex-col items-start justify-center bg-[url(/img/story-bg.png)] px-8 md:px-[15vw] md:justify-end md:pb-11"
+	>
+		<div class="mb-4">
+			<h1 class="text-xs md:text-2xl">Calidad garantizada</h1>
+			<p class="text-2xl md:text-4xl md:pr-44">
+				El barral está testeado contra intentos de apertura de todo tipo, probando que es uno de los
+				mejores productos en el mercado para cuidar tu hogar de manera accesible.
+			</p>
+		</div>
+		<div class="max-sm:hidden">
+			<ScrollArrow dark position="right" />
+		</div>
+	</div>
+	<div></div>
+</section>
--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import ProductAbout from '$lib/product/ProductAbout.svelte';
 	import ProductIntro from '$lib/product/ProductIntro.svelte';
+	import Quality from '$lib/product/Quality.svelte';
 </script>
 
 <main>
   <ProductIntro />
   <ProductAbout />
+  <Quality />
 </main>
